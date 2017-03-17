6717def1d52fee43bf2a02889c51d7c79ccd08ce
Dylan Wilson <dwilson5817@gmail.com>
2017-03-17 22:16:45 +0000

    Create basic Spigot plugin structure

---
+++ src/main/java/uk/me/dwilson/powercommand/Main.java
@@ -0,0 +1,13 @@
+package uk.me.dwilson.powercommand;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by dwils on 17/03/2017.
+ */
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("Loading PowerCommand...");
+    }
+}
